ef73c599ac2ad9db643632ef5b0f5a6cc0cee951
Juan <1766933+judavi@users.noreply.github.com>
2025-02-17 11:02:44 +0000

    Added IOS capacitor
    Added IOS capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'yogalate',
-  webDir: 'dist'
+  appId: 'com.yogaflow.app',
+  appName: 'Yoga Flow',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  ios: {
+    scheme: 'YogaFlow',
+    backgroundColor: '#ffffff'
+  }
 };
 
 export default config;
